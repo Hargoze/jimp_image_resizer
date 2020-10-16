d6427e62342f87c3664cf838045a69a31a43ab44
Hargoze <francois.lecarpentier99@gmail.com>
2020-10-16 10:39:18 +0200

    upload script

--- resize-filter.js
+++ resize-filter.js
@@ -1,15 +1,37 @@
+const fs = require("fs")
 const jimp = require('jimp')
 
-const inputFile = './source/world.png'
-const outputFile = './output/world-small-bw.jpg'
+var myArgs = process.argv.slice(2);
 
-jimp.read(inputFile)
+if (myArgs.length != 4) {
+	console.log("Invalid nb of arguments")
+	return false
+}
+
+const input = myArgs[0]
+var path = myArgs[1]
+const width = myArgs[2]
+const height = myArgs[3]
+
+if (isNaN(width) || isNaN(height)) {
+	console.log("width and length should be integer")
+	return false
+}
+fs.access(path, function(error) {
+	if (error) {
+	  console.log("Directory does not exist.")
+	  return false
+	}
+})
+if (path.slice(-1) != '/') {
+	path = path + '/'
+}
+const fileName = (input.split('.')[0].split('/'))
+const outputFile = path + fileName[fileName.length - 1] + width + 'x' + height + ".png"
+jimp.read(input)
 	.then((src) => {
 		src
-			.scaleToFit(300, 300)
-			.brightness(0.2)
-			.sepia()
-			.quality(60)
+			.resize(parseInt(width), parseInt(height))
 			.writeAsync(outputFile)
 			.then(() => {
 				console.log(`Done! Check ${outputFile}`)
